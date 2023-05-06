ff813a6ae5c0edcd4bac888afbd3eeeaaa41d9ee
jwf12 <jwf_12@hotmail.com>
2023-05-06 20:03:16 -0300

    .

--- app/settings.py
+++ app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iapl*x18)04)ooi0#!gug%^p-qzj$g=x*u*9tqmak@h22ze2(a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
